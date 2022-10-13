731b45f0ad8da018187db22d285d3885eb9ff31d
Igahsamuel <igahsamuela2@gmail.com>
2022-10-13 03:26:47 +0100

    Javascript First Exercise

---
+++ datatypes.js
@@ -0,0 +1,30 @@
+console.log(typeof name);
+console.log(typeof check);
+console.log(typeof issues);
+console.log(typeof value);
+
+let mySchool;
+let myHome;
+let isYears;
+let every;
+
+let isGood = true;
+let isBad = 'yes he is';
+let years = 34;
+let bags = 5;
+
+let firstName = 'Igah';
+let lastName = 'Abiye';
+let maritalStatus = false;
+let country = 'Nigeria';
+let age = 21;
+
+console.log(firstName, lastName, maritalStatus, country, age)
+
+let myFirstName = 'Igah', myLastName = 'Abiye', myMaritalStatus = false, myCountry = 'Nigeria', isAge = 21;
+
+console.log(myFirstName, myLastName, myMaritalStatus, myCountry, isAge);
+
+let myAge = 21;
+let yourAge = 23;
+console.log(myAge, yourAge);
---
+++ variable.js
@@ -0,0 +1,12 @@
+// comments can make code readable
+// Welcome to 30daysOfJavaScript
+/*comments can make code readable, easy to reuse and informative */
+
+const name = 'Igah';
+console.log(name);
+const check = true;
+console.log(check);
+let issues
+console.log(issues);
+const value = null;
+console.log(value);
